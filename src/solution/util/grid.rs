9b8f3ba9e0f05f903c27542f6965877c6711810a
Your Name <you@example.com>
2021-12-15 22:40:23 +0900

    remove grid::Grid

--- src/solution/util/grid.rs
+++ src/solution/util/grid.rs
@@ -1,5 +1,3 @@
-use std::ops::{Deref, DerefMut};
-
 pub const DIR4: [(isize, isize); 4] = [(0, 1), (1, 0), (0, -1), (-1, 0)];
 
 pub const DIR8: [(isize, isize); 8] = [
@@ -12,22 +10,6 @@ pub const DIR8: [(isize, isize); 8] = [
     (1, -1),
     (1, 1),
 ];
-
-pub struct Grid<S>(Vec<Vec<S>>);
-
-impl<S> Deref for Grid<S> {
-    type Target = Vec<Vec<S>>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl<S> DerefMut for Grid<S> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
 
 pub(crate) fn neighbors(
     x: usize,
