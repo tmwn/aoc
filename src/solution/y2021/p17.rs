adebd457a33265359895b4e2b37f4051aee8ad33
Your Name <you@example.com>
2021-12-16 18:40:16 +0900

    Update template (day 17)

--- src/solution/y2021/p17.rs
+++ src/solution/y2021/p17.rs
@@ -1,11 +1,16 @@
 use aocio::aocio;
 
-#[aocio]
-pub fn small(_: Vec<String>) -> i32 {
-    todo!()
+pub fn small(s: String) -> i32 {
+    solve(s.parse().unwrap()).0
+}
+
+pub fn large(s: String) -> i32 {
+    solve(s.parse().unwrap()).1
 }
 
 #[aocio]
-pub fn large(_: Vec<String>) -> i32 {
+fn solve(_: Vec<String>) -> (i32, i32) {
     todo!()
 }
+
+// aoc_test!(2021, 17, "", 0, 0);
